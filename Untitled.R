4314596afcdd97b4a209fe7ea3ee8fdd266f6cc7
Samantha MacDonald <samantha.jane.macd@gmail.com>
2023-09-11 14:23:05 -0600

    Performed an EDA on BikeShare data

--- Untitled.R
+++ Untitled.R
@@ -3,5 +3,29 @@
 ## Libraries 
 library(tidyverse) 
 library(vroom)
+library(DataExplorer)
+library(patchwork)
+library(ggplot2)
 
-## Read in the Data 
+# 1. Read in the Data 
+
+bike <- vroom("train.csv")
+
+# 2. Perform an EDA and identify key features of the dataset 
+
+variable_plot <- DataExplorer::plot_intro(bike) # visualization of glimpse! USE - 1 
+
+correlation_plot <- DataExplorer::plot_correlation(bike) # correlation heat map USE - 2 
+
+casual_plot <- casual_plot <- ggplot(data = bike, aes(x=temp, y=casual)) +  
+  geom_point() + 
+  geom_smooth()   
+
+season_plot <- ggplot(data=bike, aes(x=as.factor(season), y=count)) + geom_boxplot(fill="violet"
+
+
+plots <- (variable_plot + correlation_plot) / (casual_plot + casual_plot) 
+
+
+
+
