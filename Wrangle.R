14e8d5334d75742deba44641fdb8e03288372448
Samantha MacDonald <samantha.jane.macd@gmail.com>
2023-09-15 13:43:49 -0600

    Fit linear regression model

--- Wrangle.R
+++ Wrangle.R
@@ -9,6 +9,17 @@ bike <- vroom("train.csv")
 
 # 1. Perform at least one cleaning step using dplyr 
 
-first_clean_bikes <- bikes %>% 
-  select(-'casual')
+first_clean_bike <- bike %>% 
+  select(-c('casual', 'registered')) 
+view(first_clean_bike)
 
+# 2. Perform at least two feature enginerring steps using recipe() 
+
+my_recipe <- recipe(count~., data=bike) %>% 
+  step_date(datetime, features = "dow") %>% 
+  step_time(datetime, features=c("hour", "minute")) %>% 
+  step_zv(all_predictors()) %>% 
+  step_select(-c("casual", "registered")) 
+prepped_recipe <- prep(my_recipe)
+view(bake(prepped_recipe, new_data=bike))
+  
